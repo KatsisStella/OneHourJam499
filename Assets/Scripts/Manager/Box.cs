f4780ae5eb41d5e1f8c6818a144ba0b46854a3c8
Xwilarg <xwilarg@protonmail.com>
2024-11-16 18:53:38 +0100

    fix

---
+++ Assets/Scripts/Manager/Box.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace OneHourJam.Manager
+{
+    public class Box : MonoBehaviour
+    {
+        public bool IsDead;
+
+        private void Awake()
+        {
+            StartCoroutine(Loose());
+        }
+
+        private IEnumerator Loose()
+        {
+            yield return new WaitForSeconds(5f);
+            if (!IsDead) SceneManager.LoadScene("Lost");
+        }
+    }
+}
